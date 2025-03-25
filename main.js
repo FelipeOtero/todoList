0e608122b02227815363033b5f72bb2b71ec198b
Felipe Otero <48389979@est.ort.edu.ar>
2025-03-25 10:40:28 -0300

    hora

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-let vectorTareas=[];
-
+let vectorTareas = [];
 
 function agregarTarea() {
     let inputText = document.getElementById("escribirTarea").value;
+
     if (inputText === "") {
         console.log("Vacio");
-    }
-    else
-    {
-        let listatareas = document.getElementById("listatareas");
-        vectorTareas.push(inputText);
-        
-        listatareas.innerHTML = "";
-
-
-        listatareas.innerHTML = vectorTareas.map(tarea => `<li>${tarea}</li>`).join(""); 
+        return;
     }
 
+    let listatareas = document.getElementById("listatareas");
+
+
+    let fechaHora = new Date().toLocaleString(); 
+
+
+    vectorTareas.push({ texto: inputText, fecha: fechaHora });
+
+    document.getElementById("escribirTarea").value = "";
+
+    listatareas.innerHTML = vectorTareas
+        .map(tarea => `<li>${tarea.texto} <br> <small>${tarea.fecha}</small></li>`)
+        .join(""); 
 }
